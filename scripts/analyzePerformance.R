35c1ef5b704d0f5fae348659ecfd93341c1f9751
Domey038 <domey038@live.de>
2017-11-27 15:46:02 +0100

    Performance per compartment - analysis

--- scripts/analyzePerformance.R
+++ scripts/analyzePerformance.R
@@ -80,3 +80,64 @@ cat("HPA Performance (1:N annotation):", true/all, "\n",  sep="\t")
 cat("All:", all, "\t", "True:", true, "\t", "False:", false, "\n",  sep=" ")
 multiMapping <- table(performance_hpa$hgnc)
 cat("Average number of annotations per protein:", mean(multiMapping[multiMapping!=0]), "\n", sep=" ")
+
+
+#Calculate Performance per compartment for Swissprot 1:1 mapping
+library(data.table)
+performance_swissprot <- merge(deeploc, swissprot_merge[c("hgnc","deeploc_location")], by.x="hgnc", by.y="hgnc")
+set <- table(performance_swissprot$hgnc)==1
+subset <- performance_swissprot[performance_swissprot$hgnc %in% names(set[set]),]
+subset$Prediction <- as.character(subset$Prediction)
+subset$deeploc_location <- as.character(subset$deeploc_location)
+subset$deeploc_location <- gsub(" ", "_", subset$deeploc_location)
+pred <- data.frame(gene=subset$hgnc, compartment=subset$deeploc_location, prediction=subset$Prediction==subset$deeploc_location)
+setDT(pred)
+true <- pred[prediction==TRUE, .(true=.N), by=compartment]
+false <- pred[prediction==FALSE, .(false=.N), by=compartment]
+all <- pred[, .(all=.N), by=compartment]
+final_dt <- merge(merge(all, true, by="compartment"), false, by="compartment")
+final_dt$performance <- final_dt$true / final_dt$all
+
+library(ggplot2)
+png("deeploc.performance_per_class.swissprot_1-1.png", 1000,500, res=120)
+p <- ggplot(final_dt, aes(x=reorder(compartment, performance), y=performance, fill=compartment, width=0.5))
+p <- p + geom_bar(stat="identity",position="dodge", colour="black")
+p <- p + coord_flip()
+p <- p + xlab("")
+p <- p + ylab("Performance")
+p <- p + ylim(0,1)
+p <- p + guides(fill=FALSE)
+p <- p + theme(	axis.text.x = element_text(angle = 0, vjust = 0.5, size=12), 
+                axis.text.y = element_text(size=12), 
+                axis.title.y = element_text(size=12))
+p + ggtitle("DeepLoc performance per localization", subtitle = "Data set: Swissprot only 1:1 mapping")
+dev.off()
+
+#Calculate Performance per compartment for HPA 1:1 mapping
+performance_hpa <- merge(deeploc, hpa_merge[c("hgnc","deeploc_location")], by.x="hgnc", by.y="hgnc")
+set <- table(performance_hpa$hgnc)==1
+subset <- performance_hpa[performance_hpa$hgnc %in% names(set[set]),]
+subset$Prediction <- as.character(subset$Prediction)
+subset$deeploc_location <- as.character(subset$deeploc_location)
+subset$deeploc_location <- gsub(" ", "_", subset$deeploc_location)
+pred <- data.frame(gene=subset$hgnc, compartment=subset$deeploc_location, prediction=subset$Prediction==subset$deeploc_location)
+setDT(pred)
+true <- pred[prediction==TRUE, .(true=.N), by=compartment]
+false <- pred[prediction==FALSE, .(false=.N), by=compartment]
+all <- pred[, .(all=.N), by=compartment]
+final_dt <- merge(merge(all, true, by="compartment"), false, by="compartment")
+final_dt$performance <- final_dt$true / final_dt$all
+
+png("deeploc.performance_per_class.hpa_1-1.png", 1000,500, res=120)
+p <- ggplot(final_dt, aes(x=reorder(compartment, performance), y=performance, fill=compartment, width=0.5))
+p <- p + geom_bar(stat="identity",position="dodge", colour="black")
+p <- p + coord_flip()
+p <- p + xlab("")
+p <- p + ylab("Performance")
+p <- p + ylim(0,1)
+p <- p + guides(fill=FALSE)
+p <- p + theme(	axis.text.x = element_text(angle = 0, vjust = 0.5, size=12), 
+                axis.text.y = element_text(size=12), 
+                axis.title.y = element_text(size=12))
+p + ggtitle("DeepLoc performance per localization", subtitle = "Data set: HPA only 1:1 mapping")
+dev.off()
